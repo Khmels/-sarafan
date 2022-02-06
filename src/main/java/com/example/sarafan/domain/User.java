1a53d608be1b0edf347d930a256134428689c749
khmels <ser.khmel@gmail.com>
2022-02-06 20:45:00 +0200

    v5. Add Spring Security oAuth2. 70

---
+++ src/main/java/com/example/sarafan/domain/User.java
@@ -0,0 +1,22 @@
+package com.example.sarafan.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "usr")
+@Data
+public class User {
+    @Id
+    private String id;
+    private String name;
+    private String userPic;
+    private String email;
+    private String gender;
+    private String locale;
+    private LocalDateTime lastVisit;
+}
